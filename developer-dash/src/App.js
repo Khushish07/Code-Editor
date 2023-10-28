b83bc53450fe3dc882a4c72922e1f9daa16a115c
Krishna <kdwivedi343@gmail.com>
2023-10-28 20:58:51 +0530

    SetUp Editor-Page and RoomId-Page as a component along with routes

--- developer-dash/src/App.js
+++ developer-dash/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import EditorPage from './Components/EditorPage';
+import RoomID from './Components/RoomID';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <BrowserRouter>
+
+        <Routes>
+
+            <Route path='/' element={<RoomID/>}></Route>
+            <Route path='/editor/:roomId' element={<EditorPage/>}></Route>
+
+        </Routes>
+
+      </BrowserRouter>
+    
   );
 }
 
